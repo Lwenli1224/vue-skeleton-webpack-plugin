91190dd24e59e13cccc5e9335dbccd39612b4931
Wangyisheng <wangyisheng@baidu.com>
2017-12-25 12:53:14 +0800

    fix entry name with '-'

--- src/loader.js
+++ src/loader.js
@@ -8,11 +8,12 @@
 /* eslint-disable fecs-no-require */
 
 const loaderUtils = require('loader-utils');
+const {createHash} = require('crypto');
 const insertAt = require('./util').insertAt;
 
 const DEFAULT_LOADER_OPTIONS = {
     // template of importing skeleton component
-    importTemplate: 'import [nameCap] from \'@/pages/[nameCap].vue\';',
+    importTemplate: 'import [nameHash] from \'@/pages/[nameCap].vue\';',
     // template of route path
     routePathTemplate: '/skeleton-[name]',
     // position to insert route object in router.js file
@@ -21,6 +22,7 @@ const DEFAULT_LOADER_OPTIONS = {
 
 const ENTRY_NAME_HOLDER = /\[name\]/gi;
 const ENTRY_NAME_CAP_HOLDER = /\[nameCap\]/gi;
+const ENTRY_NAME_HASH_HOLDER = /\[nameHash\]/gi;
 
 module.exports = function (source) {
     const options = Object.assign({}, DEFAULT_LOADER_OPTIONS, loaderUtils.getOptions(this));
@@ -34,16 +36,19 @@ module.exports = function (source) {
     entry.forEach(entryName => {
         // capitalize first letter in entryName eg.skeleton -> Skeleton
         let entryCap = entryName.replace(/([a-z])(.*)/, (w, firstLetter, rest) => firstLetter.toUpperCase() + rest);
+        let entryHash = `_${(new Date()).getTime()}${createHash('md5').update(entryName + '-skeleton').digest('hex')}`;
 
         // replace placeholder in routeTpl and importTpl
-        let [skeletonRoutePath, importExpression] = [routePathTemplate, importTemplate]
-            .map(pathStr => pathStr.replace(ENTRY_NAME_HOLDER, entryName)
-                .replace(ENTRY_NAME_CAP_HOLDER, entryCap));
+        let [skeletonRoutePath, importExpression] = [routePathTemplate, importTemplate].map(
+                pathStr => pathStr.replace(ENTRY_NAME_HOLDER, entryName)
+                    .replace(ENTRY_NAME_CAP_HOLDER, entryCap)
+                    .replace(ENTRY_NAME_HASH_HOLDER, entryHash)
+            );
 
         // route object to insert
         let routeExpression = `{
             path: '${skeletonRoutePath}',
-            name: '${entryName}-skeleton',
+            name: '${entryHash}',
             component: ${entryCap}
         },`;
 
